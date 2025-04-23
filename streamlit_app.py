d4e860e8454b2b46b2321b08f4f710f852ff5edb
vitzy-ind <satoukazuma180808@gmail.com>
2025-04-23 14:56:29 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,10 @@ st.title("vitzy_ind")
 st.write(
   "halo"
 )
+st.number_input('Pick a number', 0, 10)
+st.text_input('Email address')
+st.date_input('Traveling date')
+st.time_input('School time')
+st.text_area('Description')
+st.file_uploader('Upload a photo')
+st.color_picker('Choose your favorite color')
