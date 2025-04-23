3df2c0a41a95c84f5a7e844110186cf9941dabcb
vitzy-ind <satoukazuma180808@gmail.com>
2025-04-23 15:14:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,7 @@ import streamlit as st
 st.title("vitzy_ind")
 st.write("halo")
 st.sidebar.title("Settings")
-st.sidebar.markdown("This is the sidebar content")
+st.sidebar.markdown("new add 
+1.gender"
+                   )
 st.sidebar.radio("gender",["laki","cewe"])
