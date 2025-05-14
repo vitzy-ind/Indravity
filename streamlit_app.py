fdeb763d8b6cd1acce7784674ea23a7347996030
vitzy-ind <satoukazuma180808@gmail.com>
2025-05-14 15:20:52 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import streamlit as st
+oh import streamlit as st
 
 st.title("Indra")
 st.write("menjalani hari hari biasa dengan bersekolah")
@@ -12,5 +12,14 @@ st.markdown("ini test bawah")
 st.subheader("ini test mid")
 st.caption("ini buat caption nya")
 st.code("x = 2025")
-st.latex(r''' 9 - x² menghasilkan y= x+79 - x² = x + 7Maka x = 1 dan x = -2 V = π112(9-x2)² - (x + 7)2dx V= π12(x4-18x²+81)-(x² +14x +49)dx V = π/12(x4 - 19x2 - 14x + 32)dx19 V = x[-x-12 y= x+79 - x² = x + 7 Maka x = 1 dan x = -2 V = π112(9-x2)² - (x + 7)2dx
-V = π12(x4-18x²+81)-(x² +14x +49)dx V = π/12(x4 - 19x2 - 14x + 32)dx 19 V = x[-x-12x3-7 y= x+79 - x² = x + 7 Maka x = 1 dan x = -2 V = π112(9-x2)² - (x + 7)2dx V = π12(x4-18x²+81)-(x² +14x +49)dx V = π/12(x4 - 19x2 - 14x + 32) ''')
+st.latex(r''' 9 - x² menghasilkan y= x+79 - x² = x + 7Maka
+x = 1 dan x = -2 V = π112(9-x2)² - (x + 7)2dx 
+V= π12(x4-18x²+81)-(x² +14x +49)dx 
+V = π/12(x4 - 19x2 - 14x + 32)dx19 
+V = x[-x-12 y= x+79 - x² = x + 7 Maka x = 1 dan x = -2 
+V = π112(9-x2)² - (x + 7)2dx
+V = π12(x4-18x²+81)-(x² +14x +49)dx V = π/12(x4 - 19x2 - 14x + 32)dx 19 
+V = x[-x-12x3-7 y= x+79 - x² = x + 7 
+Maka x = 1 dan x = -2 V = π112(9-x2)² - (x + 7)2dx
+V = π12(x4-18x²+81)-(x² +14x +49)dx
+V = π/12(x4 - 19x2 - 14x + 32) ''')
