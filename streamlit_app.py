cde84b04cb075e028a1ec221421b1b310e347dc3
vitzy-ind <satoukazuma180808@gmail.com>
2025-05-14 14:30:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("Indra")
-st.write("halo")
+st.write("menjalani hari hari biasa dengan bersekolah")
 st.sidebar.title("Settings")
 st.sidebar.markdown("new add")
 st.sidebar.markdown("gender")
