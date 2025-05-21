18b8fecd3fec4388eb439244e39d662225a1e8ea
vitzy-ind <satoukazuma180808@gmail.com>
2025-05-21 14:28:43 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,1 @@
-st.write("menjalani hari hari biasa dengan bersekolah")
-st.sidebar.title("Settings")
-st.sidebar.markdown("new add")
-st.sidebar.markdown("gender")
-st.sidebar.radio("gender",["male","female"])
-st.title("cuma test aja ")
-st.header("test mid")
-st.markdown("ini test bawah")
-st.subheader("ini test mid")
-st.caption("ini buat caption nya")
-st.code("x = 2025")
+
