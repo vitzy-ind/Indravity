a59a8c2fa801e8be462449bbd5e4b7f16d2e9f03
vitzy-ind <satoukazuma180808@gmail.com>
2025-05-21 14:32:25 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,1 @@
-st.checkbox('Yes')
-st.button('Click Me')
-st.radio('Pick your gender', ['Male', 'Female'])
-st.selectbox('Pick a fruit', ['Apple', 'Banana', 'Orange'])
-st.multiselect('Choose a planet', ['Jupiter', 'Mars', 'Neptune'])
-st.select_slider('Pick a mark', ['Bad', 'Good', 'Excellent'])
-st.slider('Pick a number', 0, 50)
+l
